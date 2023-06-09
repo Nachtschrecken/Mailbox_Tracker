6dd76789d85741edc7f3fb1b40f786843e532d9e
Nachtschrecken <ferris@uldagon.com>
2023-06-09 14:40:22 +0200

    first commit

---
+++ cred.py
@@ -0,0 +1,6 @@
+SSID = "Your Wifi Name"
+PASS = "Your Wifi Password"
+GOOG = "Your Google Mail App Password (or other credential, this is for Gmail)"
+
+# Notice that Gmail App Password is NOT your Google Password
+# Search for "Gmail App Password" to see more
---
+++ main.py
@@ -0,0 +1,43 @@
+import cred
+import umail
+import network
+import utime
+from utime import sleep
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(cred.SSID, cred.PASS)
+while wlan.isconnected() == False:
+    sleep(1)
+
+def sendmail():
+    sender_email = 'kleierferris@gmail.com'
+    sender_name = 'Algalon Mailsystem'
+    sender_app_password = cred.GOOG
+    recipient_email ='ferris@uldagon.com'
+    email_subject ='You got Mail'
+
+    smtp = umail.SMTP('smtp.gmail.com', 465, ssl=True)
+    smtp.login(sender_email, sender_app_password)
+    smtp.to(recipient_email)
+    smtp.write("From:" + sender_name + "<"+ sender_email+">\n")
+    smtp.write("Subject:" + email_subject + "\n")
+    
+    current_time = utime.localtime()
+    formatted_time = "{:02d}:{:02d}:{:02d}".format(current_time[3], current_time[4], current_time[5])
+    smtp.write("\nHello Ferris" + "\n\nYou got new mail at " + formatted_time + ".\nCheck your mailbox")
+    
+    smtp.send()
+    smtp.quit()
+    
+led = machine.Pin("LED", machine.Pin.OUT)
+pin = machine.Pin(28, machine.Pin.IN)
+
+if pin.value():
+    for x in range(15):
+        led.toggle()
+        sleep(0.01)
+        
+for x in range(16):
+    led.toggle()
+    sleep(0.2)
